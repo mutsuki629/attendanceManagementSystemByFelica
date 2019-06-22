5ee9f2643ba947b6ffbe6647e6adf650808313ec
Mitsutoshi Ura <mitsutoshi@Mu-Macbook-Air.local>
2019-06-22 20:34:29 +0900

    2019/06/22 20:34

--- module/nfc_reader.py
+++ module/nfc_reader.py
@@ -1,16 +1,8 @@
 import binascii
-
 import nfc
 
-def connected(tag):
-    pass
+def main():
+    print(123)
 
 if __name__=='__main__':
-    with nfc.ContactlessFrontend as clf:
-        print(clf)
-        if clf:
-            while True:
-                tag = clf.connect(rdqr={
-                    'on-connect': lambda tag: False,
-                })
-            print(tag)
+    main()
