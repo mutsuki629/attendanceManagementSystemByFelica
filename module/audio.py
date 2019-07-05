30f708def16ce295777efda74800771866f9be09
Mitsutoshi Ura <mitsutoshi@Mu-Macbook-Air.local>
2019-07-05 18:52:51 +0900

    n

--- module/audio.py
+++ module/audio.py
@@ -1,20 +1,3 @@
-import wave
-import pyaudio
+import subprocess
 
-beepLoca = 'audio/click.wav'
-
-def beep(filename):
-    wf = wave.open(filename, "r")
-    # ストリーム開始
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
-
-    data = wf.readframes(1024)
-    while(data != ''):
-        stream.write(data)
-        data = wf.readframes(1024)
-    stream.close()      # ストリーム終了
-    p.terminate()
+subprocess.call('cvlc beep.wav')
