ac6bf3c202b57c436c1a60323e37f617c8b79a8f
Mitsutoshi Ura <mitsutoshi@Mu-Macbook-Air.local>
2019-07-05 17:08:33 +0900

    test

---
+++ module/audio.py
@@ -0,0 +1,20 @@
+import wave
+import pyaudio
+
+beepLoca = 'audio/click.wav'
+
+def beep():
+    wf = wave.open(beepLoca, "r")
+    # ストリーム開始
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(1024)
+    while(data != ''):
+        stream.write(data)
+        data = wf.readframes(1024)
+    stream.close()      # ストリーム終了
+    p.terminate()
